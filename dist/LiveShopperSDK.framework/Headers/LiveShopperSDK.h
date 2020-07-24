6d8872093283c5c5e3e71cdb1fdf22bf2ef13a58
Shaun Mason <shaun@liveshopper.com>
2020-07-24 13:02:03 -0400

    feat: modify for deployment

---
+++ dist/LiveShopperSDK.framework/Headers/LiveShopperSDK.h
@@ -0,0 +1,6 @@
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double const LiveShopperSDKVersionNumber;
+FOUNDATION_EXPORT const unsigned char SLiveShopperSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiveShopeprSDK/PublicHeader.h
